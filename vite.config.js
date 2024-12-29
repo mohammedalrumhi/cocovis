bfa13366acf1603f706731eefcc7b214df8f782b
mohammedalrumhi <mohammed.alrumhi@outlook.com>
2024-12-30 01:45:19 +0400

    sdsa

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   outDir: 'dist'
+  build: {
+    outDir: 'dist',  // Output directory for the build
+    sourcemap: true, // Generate source maps for easier debugging
+  },
 })
